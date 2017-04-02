2694b03d77dfe62e9b11d3a1751d55b5938fedaf
Matt Giltaji <matt@giltaji.com>
2017-04-02 01:49:23 -0700

    area names should flow through

--- filterManuel.go
+++ filterManuel.go
@@ -2,9 +2,12 @@ package main
 
 import "regexp"
 
-var sectionSeparator = regexp.MustCompile(`^=====*$`)
+var (
+	sectionSeparator = regexp.MustCompile(`^=====*$`)
+	sectionHeader = regexp.MustCompile(`^\[[A-Z]+.*\]$`)
+)
 
 func ShouldCopy(s string, allowed []string) bool {
 	_ = allowed
-	return sectionSeparator.MatchString(s)
+	return sectionSeparator.MatchString(s) || sectionHeader.MatchString(s)
 }
--- filterManuel_test.go
+++ filterManuel_test.go
@@ -7,17 +7,31 @@ func TestShouldCopy(t *testing.T) {
 		in   string
 		want bool
 	}{
+		//good section separators
 		{"=================================", true},     //real line separator
 		{"=====================================", true}, //longer
 		{"==========", true},                            //arbitrarily short but valid
 		{"====", true},                                  //shortest possible valid
-		{"=", false},                                    //too short
-		{"==", false},                                   //also too short
-		{"===", false},                                  //still too short
-		{"---------------------------", false},          //wrong symbol
-		{"-=-=-=-=-=-=-=-=-=-=", false},                 //no mixing
-		{"----=======-----", false},                     //nope
-		{"============================-", false},        //no dash
+		//bad section separators
+		{"=", false},                             //too short
+		{"==", false},                            //also too short
+		{"===", false},                           //still too short
+		{"---------------------------", false},   //wrong symbol
+		{"-=-=-=-=-=-=-=-=-=-=", false},          //no mixing
+		{"----=======-----", false},              //nope
+		{"============================-", false}, //no dash
+
+		//good area names
+		{"[Ye Olde Medievale Villagee]", true},                  // plain name
+		{"[An Incredibly Strange Place (Mediocre Trip)]", true}, //parentheses
+		{"[Anger Man's Level]", true},                           //apostrophe
+		{"[The Gourd!]", true},                                  //bang!
+		{"[LavaCo™ Lamp Factory]", true},                        //trademark
+		{"[A Deserted Stretch of I-911]", true},                 //dash
+		{"[Engineering]", true},                                 // no space in area name
+		//bad area names
+		{"[]", false}, {"[ ]", false},      //blanks alone insufficient
+		{"[!]", false}, {"[*****]", false}, //alphabetical rune needed
 
 	}
 	for _, c := range cases {
